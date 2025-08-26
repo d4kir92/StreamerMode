8e8bf8c39b00475047cf0fbca4fa5617cb0e5915
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2025-08-26 21:44:10 +0500

    Create ruRU.lua

---
+++ locale/ruRU.lua
@@ -0,0 +1,6 @@
+-- ruRU Russian
+local _, StreamerMode = ...
+StreamerMode:AddTrans("ruRU", "LID_GENERAL", "Общее")
+StreamerMode:AddTrans("ruRU", "LID_SHOWMINIMAPBUTTON", "Показать кнопку на миникарте")
+StreamerMode:AddTrans("ruRU", "LID_charname", "Имя Вашего персонажа")
+StreamerMode:AddTrans("ruRU", "LID_HIDECHARACTERNAME", "Скрыывть имя Вашего персонажа")
