c968f4d0a71f969447482ff554749cea0ffc91e8
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-09-27 13:40:14 +0200

    v1.1.12

--- core.lua
+++ core.lua
@@ -242,7 +242,7 @@ local function Init()
 		STMOTABPC = STMOTABPC or {}
 		STMOTABPC["charname"] = STMOTABPC["charname"] or "RENAMEME"
 		SM_CHARNAME = STMOTABPC["charname"]
-		StreamerMode:SetVersion(132150, "1.1.11")
+		StreamerMode:SetVersion(132150, "1.1.12")
 		StreamerMode:SetAddonOutput("StreamerMode", 132150)
 		StreamerMode:CreateMinimapButton(
 			{
